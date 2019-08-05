a65b432d0ed77c639350b8a5272f7bc4ec0dd83a
Yann Lugrin <yann.lugrin@antistatique.net>
2019-08-06 00:06:16 +0300

    Remove Discord rules reaction when a member leaves the server

--- src/bot/events/discordRules.js
+++ src/bot/events/discordRules.js
@@ -1,7 +1,10 @@
+const { RuntimeError } = require('constants');
+
 module.exports = {
   settings: [
     { key: 'discordRulesEnabled', type: 'Boolean', permissions: ['ADMINISTRATOR'], default: false },
     { key: 'discordRulesMessage', type: 'String', permissions: ['ADMINISTRATOR'] },
+    { key: 'discordRulesChannel', type: 'String', permissions: ['ADMINISTRATOR'], internal: true, cached: true },
     { key: 'discordRulesReaction', type: 'String', permissions: ['ADMINISTRATOR'] },
     { key: 'discordRulesPromote', type: 'Role', permissions: ['ADMINISTRATOR'] },
   ],
@@ -22,11 +25,40 @@ module.exports = {
         if (reaction.message.id !== await settings.get('discordRulesMessage')) return;
         if (emoji !== await settings.get('discordRulesReaction')) return;
 
+        // Save message channel id to be able to retrive it later
+        await settings.get('discordRulesChannel').then((value) => {
+          if (value !== reaction.message.channel.id) return settings.set('discordRulesChannel', reaction.message.channel.id);
+        });
+
         return reaction.message.guild.fetchMember(user)
           .then(async (member) => {
             return member.addRole(await settings.get('discordRulesPromote'));
           });
       }
     },
+    {
+      name: 'guildMemberRemove',
+      description: 'Remove user reaction on Discord rules on leave',
+      async execute(member) {
+        const settings = await this.getSettings(member.guild);
+
+        return settings.get('discordRulesMessage').then(async (messageID) => {
+          if (!messageID) return;
+
+          const message = await settings.get('discordRulesChannel').then((channelID) => {
+            if (!channelID) throw new RuntimeError(`discordRulesChannel setting is not set`);
+            return member.guild.channels.get(channelID).fetchMessage(messageID);
+          });
+
+          const rulesEmoji = await settings.get('discordRulesReaction');
+          const reaction = message.reactions.find((reaction) => {
+            const emoji = reaction.emoji.id ? `${reaction.emoji.name}:${reaction.emoji.id}` : reaction.emoji.name;
+            return emoji === rulesEmoji;
+          });
+
+          return reaction ? reaction.remove(member) : undefined;
+        });
+      }
+    }
   ]
 };
