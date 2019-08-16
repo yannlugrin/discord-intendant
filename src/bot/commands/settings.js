bbdbdb70b73ac55e2b39ac734ff26e13294fc665
Yann Lugrin <yann.lugrin@antistatique.net>
2019-08-16 10:36:11 +0200

    Fix !settings command when value is not valid

--- src/bot/commands/settings.js
+++ src/bot/commands/settings.js
@@ -3,13 +3,18 @@ module.exports = {
   description: 'Display all guild settings',
   async execute(message, settings) {
     const values = [''];
+    const keys = Array.from(settings.definitions.keys()).sort();
 
-    for (const [key] of settings.definitions) {
-      const value = await settings.formatted(key, message);
-      if (value === undefined) {
-        values.push(`Value of '${key}' is unset`);
-      } else {
-        values.push(`Value of '${key}' is set to: ${value}`);
+    for (const key of keys) {
+      try {
+        const value = await settings.formatted(key, message);
+        if (value === undefined) {
+          values.push(`Value of '*${key}*' is empty`);
+        } else {
+          values.push(`Value of '*${key}*' is set to: **${value}**`);
+        }
+      } catch {
+        values.push(`**Value of '${key}' is not valid**`);
       }
     }
 
